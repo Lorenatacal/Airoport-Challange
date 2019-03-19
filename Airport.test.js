dfaf8efe02f203bc9f51a6ba2003ceaab405e45a
Lorenatacal <Lorenatacal@gmail.com>
2019-03-19 23:26:06 +0000

    Create a loop that  creates planes im the test that checks the airport capacity

--- Airport.test.js
+++ Airport.test.js
@@ -37,22 +37,19 @@ describe("Airport", () => {
         });
         test('should not allow a plane to land when the airport is full', () => {   
             let randomWeather = () => {};
-            let plane = createPlane();
-            let plane1 = createPlane();
-            let plane2 = createPlane();
-            let plane3 = createPlane();
-            let plane4 = createPlane();
-            let plane5 = createPlane();
-            // refactor this to use a loop to generate the planes
             let airportLuton = createAirport();
+            let planes = [];
+            [1, 2, 3, 4, 5, 6].forEach(function(elem){
+                planes.push(createPlane());
+            });
             let consoleSpy = jest.spyOn(console, 'log');
 
-            land(airportLuton, plane, randomWeather);
-            land(airportLuton, plane1, randomWeather);
-            land(airportLuton, plane2, randomWeather);
-            land(airportLuton, plane3, randomWeather);
-            land(airportLuton, plane4, randomWeather);
-            land(airportLuton, plane5, randomWeather);
+            land(airportLuton, planes[0], randomWeather);
+            land(airportLuton, planes[1], randomWeather);
+            land(airportLuton, planes[2], randomWeather);
+            land(airportLuton, planes[3], randomWeather);
+            land(airportLuton, planes[4], randomWeather);
+            land(airportLuton, planes[5], randomWeather);
             //  refactor this to use a loop to call land
             expect(airportLuton.planes.length).toEqual(5);
             expect(consoleSpy).toHaveBeenCalledWith('We wont land, the airport is full');
@@ -102,9 +99,7 @@ describe("Airport", () => {
         test('should remove a specific plane from the airport', () => {
             let randomWeather = () => {};
             let plane = createPlane();
-            plane.flying = false;
             let plane1 = createPlane();
-            plane1.flying = false;
             let plane2 = createPlane();
             plane2.flying = false;
             // make this dry, use a loop to generate and set the flag
@@ -114,7 +109,6 @@ describe("Airport", () => {
 
             let consoleSpy = jest.spyOn(console, 'log')
             takeOff(airportLondon, plane2, randomWeather);
-            console.log(airportLondon, 'air'); // don't leave redundant console logs in the code
             
             expect(airportLondon.planes.length).toEqual(2);
             expect(airportLondon.planes.includes(plane2)).toEqual(false);
